65e966a57ff457ebd3a5cb231d64f7c76569d5a1
vongphachan~ <vongphachannukkalash@gmail.com>
2023-07-03 11:54:53 +0700

    atteched objectId to next auth session user data

--- src/configs/nextAuth.ts
+++ src/configs/nextAuth.ts
@@ -1,5 +1,9 @@
+import { COLLECTIONS_USERS } from "@/constants";
+import clientPromise from "@/startups/mongodb";
+import { ObjectId } from "mongodb";
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/google";
+import jwt from "jsonwebtoken";
 
 export const nextAuthOptions: NextAuthOptions = {
   session: {
@@ -7,33 +11,62 @@ export const nextAuthOptions: NextAuthOptions = {
   },
   providers: [
     CredentialsProvider({
+      name: "Google",
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-      /*    profile: async (_profile: any, token: any) => {
-        const profile = _profile as GoogleProfile;
+    }),
+  ],
+  jwt: {
+    encode: async ({ secret, token, maxAge }) => {
+      if (!token) return "";
 
-        const mongoClient = await clientPromise;
+      const mongoClient = await clientPromise;
 
-        const existingUser = await mongoClient
-          .db(process.env.DATABASE_NAME)
-          .collection(COLLECTIONS_USERS)
-          .findOne({
-            email: profile.email,
-          });
+      let existingUser = await mongoClient
+        .db(process.env.DATABASE_NAME)
+        .collection(COLLECTIONS_USERS)
+        .findOne({
+          email: token.email,
+        });
 
-        if (existingUser) return _profile;
-
-        await mongoClient
+      if (!existingUser) {
+        const insertResult = await mongoClient
           .db(process.env.DATABASE_NAME)
           .collection(COLLECTIONS_USERS)
           .insertOne({
-            email: profile.email,
-            name: profile.name,
-            image: profile.picture,
+            email: token.email,
+            name: token.name,
+            image: token.picture,
           });
 
-        return _profile;
-      }, */
-    }),
-  ],
+        if (!insertResult.acknowledged || !insertResult.insertedId) return "";
+
+        existingUser = await mongoClient
+          .db(process.env.DATABASE_NAME)
+          .collection(COLLECTIONS_USERS)
+          .findOne({ _id: new ObjectId(insertResult.insertedId) });
+      }
+
+      const salt = jwt.sign({ ...existingUser }, secret, { expiresIn: maxAge });
+
+      return salt;
+    },
+    decode: async ({ secret, token }) => {
+      const decoded = jwt.verify(token!, secret) as any;
+
+      return {
+        ...decoded,
+        _id: (decoded as any)._id || "",
+      };
+    },
+  },
+  callbacks: {
+    async session({ session, token }) {
+      (session.user as any)._id = token._id;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
 };
