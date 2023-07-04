f87725ddbb6ff52d09f92b0033a484a0d3a76fa7
vongphachan~ <vongphachannukkalash@gmail.com>
2023-07-04 11:49:27 +0700

    handled auto focus on input

--- src/app/messagePanel/footer.tsx
+++ src/app/messagePanel/footer.tsx
@@ -7,7 +7,7 @@ import {
 } from "@/hooks";
 import Image from "next/image";
 import microphoneIcon from "../send.svg";
-import { useContext, useRef, useState, useCallback } from "react";
+import { useContext, useRef, useState, useCallback, useEffect } from "react";
 import { UserContext } from "../context";
 import { AuthenticationGateContext } from "@/components/authenticationGate";
 import { MessageContext } from "./context";
@@ -48,36 +48,51 @@ const Footer = () => {
   const send = useCallback(() => {
     sendMessage();
     setContent("");
-    inputRef.current?.focus();
-  }, [inputRef, sendMessage]);
+  }, [sendMessage]);
+
+  useEffect(() => {
+    const ref = inputRef.current;
+
+    if (!ref) return;
+
+    ref?.focus();
+
+    return () => ref?.blur();
+  });
 
   return (
-    <footer className="flex gap-4 items-center mt-auto justify-between py-4 px-8 bg-blue-950">
-      <div className=" rounded-3xl w-full overflow-hidden">
-        <input
-          ref={inputRef}
-          className=" text-sm w-full h-12 px-4 outline-none"
-          onChange={(event) => setContent(event.target.value)}
-          disabled={loading || !!error}
-          value={content}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") send();
-          }}
-        />
-      </div>
-      <button
-        className=" bg-blue-800 hover:bg-blue-900 transition-colors px-2 rounded-full shrink-0 w-[48px] h-[48px] flex justify-center items-center"
-        disabled={loading || !!error}
-        onClick={() => send()}
+    <footer className="bg-blue-950">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          send();
+        }}
+        className="flex gap-4 items-center mt-auto justify-between py-4 px-8"
       >
-        <Image
-          src={microphoneIcon}
-          width={24}
-          height={24}
-          className=" translate-x-[4px]"
-          alt="Microfone"
-        />
-      </button>
+        <div className=" rounded-3xl w-full overflow-hidden">
+          <input
+            ref={inputRef}
+            className=" text-sm w-full h-12 px-4 outline-none"
+            onChange={(event) => setContent(event.target.value)}
+            disabled={loading || !!error}
+            value={content}
+          />
+        </div>
+        <button
+          type="submit"
+          className=" bg-blue-800 hover:bg-blue-900 transition-colors px-2 rounded-full shrink-0 w-[48px] h-[48px] flex justify-center items-center"
+          disabled={loading || !!error}
+          onClick={() => send()}
+        >
+          <Image
+            src={microphoneIcon}
+            width={24}
+            height={24}
+            className=" translate-x-[4px]"
+            alt="Microfone"
+          />
+        </button>
+      </form>
     </footer>
   );
 };
