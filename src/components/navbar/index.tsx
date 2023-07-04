f7665e31b8dac3b809662d48c075b40857ec6062
vongphachan~ <vongphachannukkalash@gmail.com>
2023-07-04 23:04:18 +0700

    removed unused component and fixed profile disappear on small screen

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -18,39 +18,8 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-          <div className="hidden md:block">
+          <div>
             <div className="ml-4 flex items-center md:ml-6">
-              <button
-                type="button"
-                className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-              >
-                <span className="sr-only">View notifications</span>
-
-                {/* Heroicon name: outline/bell */}
-
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 3h18v18H3V3z"
-                  />
-
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M7 14v-3c0-1.1.9-2 2-2s2 .9 2 2v3m6 0v-3c0-1.1.9-2 2-2s2 .9 2 2v3"
-                  />
-                </svg>
-              </button>
               <ProfileButton />
             </div>
           </div>
