8a8f46a5be637dab78fac72bd4400b70a7548dc0
vongphachan~ <vongphachannukkalash@gmail.com>
2023-07-06 13:55:45 +0700

    signin buttons will get callback url from env

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,27 +5,36 @@ import googleIcon from "./google.svg";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 
+const SignInButton = (props: {
+  provider: "google" | "facebook";
+  children: React.ReactNode;
+}) => {
+  return (
+    <button
+      className=" bg-blue-300 hover:bg-blue-800 transition-colors p-2 rounded-full"
+      onClick={() =>
+        signIn(props.provider, {
+          callbackUrl: process.env.NEXT_PUBLIC_SIGN_IN_CALLBACK_URL,
+        })
+      }
+    >
+      {props.children}
+    </button>
+  );
+};
+
 const LoginPage = () => {
   return (
     <div className="h-screen w-full bg-blue-950">
       <div className="flex flex-col gap-12 items-center h-full pt-[10%]">
         <h1 className=" text-xl text-blue-500 font-medium">Login with ...</h1>
         <div className=" w-full sm:w-96 flex justify-evenly">
-          <button
-            className=" bg-blue-300 hover:bg-blue-800 transition-colors p-2 rounded-full"
-            onClick={() => signIn("google", { callbackUrl: "/" })}
-          >
+          <SignInButton provider="google">
             <Image width={64} src={googleIcon} alt="google icon" />
-          </button>
-          <button
-            disabled
-            className=" opacity-20 bg-blue-300 p-2 rounded-full"
-            onClick={() =>
-              signIn("google", { callbackUrl: "http://localhost:3000" })
-            }
-          >
+          </SignInButton>
+          <SignInButton provider="facebook">
             <Image width={64} src={facebookIcon} alt="facebook icon" />
-          </button>
+          </SignInButton>
         </div>
       </div>
     </div>
